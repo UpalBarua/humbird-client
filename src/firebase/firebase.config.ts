012f2d93994be457c7dfa689f98e559a71f4d332
Upal Barua <upalbarua52@gmail.com>
2023-03-13 19:56:35 +0600

    added .env.local

--- src/firebase/firebase.config.ts
+++ src/firebase/firebase.config.ts
@@ -1,12 +1,12 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCgqRIyViKnS6HYxenoC2GUnYAaAK0KkoA',
-  authDomain: 'humbird-a1458.firebaseapp.com',
-  projectId: 'humbird-a1458',
-  storageBucket: 'humbird-a1458.appspot.com',
-  messagingSenderId: '473185572347',
-  appId: '1:473185572347:web:29649db6d26321d5fea75e',
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_FIREBASE_APPID,
 };
 
 export const app = initializeApp(firebaseConfig);
